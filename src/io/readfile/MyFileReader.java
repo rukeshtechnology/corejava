98871fda79f6a597cc33cf88e2b4412b6db188e0
rukesh <rukesh@LAPTOP-NF11M533.home>
2017-11-13 15:00:50 -0500

    File Reader.

--- src/io/readfile/MyFileReader.java
+++ src/io/readfile/MyFileReader.java
@@ -1,4 +1,4 @@
-package reading.files;
+package io.readfile;
 
 import java.io.BufferedReader;
 import java.io.File;
--- src/io/readfile/MyFileReaderDriver.java
+++ src/io/readfile/MyFileReaderDriver.java
@@ -1,4 +1,4 @@
-package reading.files;
+package io.readfile;
 
 public class MyFileReaderDriver {
 	
